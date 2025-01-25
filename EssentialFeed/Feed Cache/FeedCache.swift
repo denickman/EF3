37fdd26f907df86d00fe1847fb086d5e041bdc6c
Dennis Yaremenko <yaremdennis@gmail.com>
2025-01-25 12:43:15 +0200

    [Persistance]: Proper Memory-Management of Captured References Within Deeply Nested Closures + Identifying Highly-Coupled Modules

---
+++ EssentialFeed/Feed Cache/FeedCache.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCache.swift
+//  EssentialFeed
+//
+//  Created by Denis Yaremenko on 25.01.2025.
+//
+
+import Foundation
+
+// (abstraction)
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
